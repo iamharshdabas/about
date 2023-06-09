d55aff785efc81b14778ccae0e0e3039f7807a5c
Harsh Dabas <iamharshdabas@gmail.com>
2023-06-09 19:10:56 +0530

    UP: ThreeJs values

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  75,
+  45,
   window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+  1,
+  100
 );
 
 const renderer = new THREE.WebGLRenderer();
@@ -22,7 +22,7 @@ camera.position.z = 5;
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
+  // cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
 
   renderer.render(scene, camera);
