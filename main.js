34edf6b98cb8eb1fadacd32859914d8bb15e4a7a
Harsh Dabas <iamharshdabas@gmail.com>
2023-06-11 11:29:22 +0530

    ADD: ThreeJs star background

---
+++ main.js
@@ -0,0 +1,20 @@
+import * as THREE from 'three'
+
+const width = innerWidth
+const height = innerHeight
+
+const screen = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(80, width / height, 1, 80)
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(width, height)
+document.body.appendChild(renderer.domElement)
+
+// make a sphere
+const geometry = new THREE.SphereGeometry(0.02, 32, 32)
+const material = new THREE.MeshBasicMaterial({ color: 0xffffff })
+const sphere = new THREE.Mesh(geometry, material)
+screen.add(sphere)
+
+camera.position.z = 2
+renderer.render(screen, camera)
