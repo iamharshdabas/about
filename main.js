6e87cdae2db43bafd9e5386112731924ecd14a3d
Harsh Dabas <iamharshdabas@gmail.com>
2023-06-11 12:34:00 +0530

    ADD: sphere animation

--- main.js
+++ main.js
@@ -5,15 +5,22 @@ const height = innerHeight
 
 const screen = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(80, width / height, 1, 80)
-
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(width, height)
 document.body.appendChild(renderer.domElement)
 
-const geometry = new THREE.SphereGeometry(0.02, 32, 32)
+const geometry = new THREE.SphereGeometry(1, 8, 8)
 const material = new THREE.MeshBasicMaterial({ color: 0xffffff })
 const sphere = new THREE.Mesh(geometry, material)
 screen.add(sphere)
 
 camera.position.z = 2
-renderer.render(screen, camera)
+
+function animate() {
+  requestAnimationFrame(animate)
+  sphere.rotation.x += 0.01
+  sphere.rotation.y += 0.01
+  renderer.render(screen, camera)
+}
+
+animate()
