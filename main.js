64bad979085d8e129db273649f7a6dd6b4527b3c
Harsh Dabas <iamharshdabas@gmail.com>
2023-06-11 13:21:48 +0530

    UP: sphere point size

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ const renderer = new THREE.WebGLRenderer()
 renderer.setSize(width, height)
 document.body.appendChild(renderer.domElement)
 
-const geometry = new THREE.SphereGeometry(1, 24, 24)
-const material = new THREE.PointsMaterial({ color: 0xffffff, size: 0.02 })
+const geometry = new THREE.SphereGeometry(1, 64, 64)
+const material = new THREE.PointsMaterial({ color: 0xffffff, size: 0.002 })
 const sphere = new THREE.Points(geometry, material)
 screen.add(sphere)
 
@@ -18,8 +18,9 @@ camera.position.z = 2
 
 function animate() {
   requestAnimationFrame(animate)
-  sphere.rotation.x += 0.01
-  sphere.rotation.y += 0.01
+  sphere.rotation.x += 0.002
+  sphere.rotation.y += 0.002
+  sphere.rotation.z += 0.002
   renderer.render(screen, camera)
 }
 
