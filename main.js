4398e067d5925ac3212ece4e1fe68517317ce3b2
Harsh Dabas <iamharshdabas@gmail.com>
2023-06-12 16:04:35 +0530

    UP: star background values

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const height = innerHeight
 
 const screen = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(80, width / height, 1, 80)
+camera.position.z = 2
+
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(width, height)
 document.body.appendChild(renderer.domElement)
@@ -13,14 +15,14 @@ const totalStar = 2000
 const starGeomatry = new THREE.BufferGeometry()
 const stars = new Float32Array(totalStar * 3)
 for (let i = 0; i < totalStar * 3; i++) {
-  stars[i] = Math.random() * 4 - 2
+  stars[i] = Math.random() * 24 - 12
   starGeomatry.setAttribute('position', new THREE.BufferAttribute(stars, 3))
 }
+
 const starMaterial = new THREE.PointsMaterial({ color: 0xffffff, size: 0.008 })
+
 const starMesh = new THREE.Points(starGeomatry, starMaterial)
 screen.add(starMesh)
-
-camera.position.z = 2
 
 function animate() {
   requestAnimationFrame(animate)
